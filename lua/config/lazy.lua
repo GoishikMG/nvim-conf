873d2c032fb7171fc439fc644641be34299bef14
Maxim <goishikmg@tut.by>
2024-11-03 12:40:02 +0300

    Install telescope

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -12,7 +12,12 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-	{
+    {
+        'nvim-telescope/telescope.nvim',
+        tag = '0.1.4',
+        dependencies = {'nvim-lua/plenary.nvim'}
+    },
+    {
 		"nvim-neo-tree/neo-tree.nvim",
 		branch = "v3.x",
 		dependencies = {
