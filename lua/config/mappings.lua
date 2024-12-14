68017c28cd29b8dedad2bd7a6aa4a436d1e3710b
Maxim <goishikmg@tut.by>
2024-12-14 14:42:21 +0300

    Center search view

--- lua/config/mappings.lua
+++ lua/config/mappings.lua
@@ -8,3 +8,5 @@ vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle)
 
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
