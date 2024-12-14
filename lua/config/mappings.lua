d91b40508e75bc392def1a6d5e43aefa6f4801c6
Maxim <goishikmg@tut.by>
2024-12-14 14:13:27 +0300

    Center jumps view

--- lua/config/mappings.lua
+++ lua/config/mappings.lua
@@ -5,3 +5,6 @@ vim.keymap.set('n', '<leader>E', ':Neotree left reveal<CR>')
 vim.keymap.set('n', '<leader>o', ':Neotree float git_status<CR>')
 
 vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle)
+
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
