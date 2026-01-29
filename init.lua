675eacb799db7bdb1777a2e22f67050b7687d02f
Maxim <goishikmg@tut.by>
2026-01-29 16:44:05 +0300

    Migrate to new lsconfig

--- init.lua
+++ init.lua
@@ -5,7 +5,6 @@ require("config.colors")
 
 require("plugins.treesitter")
 require("plugins.neotree")
-require("plugins.lsp")
 require("plugins.cmp")
 require("plugins.mason")
 require("plugins.telescope")
@@ -15,3 +14,21 @@ require("plugins.autotag")
 require("plugins.gitsigns")
 require("plugins.hop")
 require("plugins.harpoon")
+
+vim.lsp.enable({
+    "phpactor",
+    "ts_ls",
+    "html",
+    "cssls",
+    "gopls"
+})
+
+vim.diagnostic.config({
+  virtual_text = true,
+  signs = true,
+  update_in_insert = false,
+  underline = true,
+  severity_sort = false,
+  float = true,
+})
+
