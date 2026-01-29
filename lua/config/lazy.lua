a3b131ccc5c7b64032ebab1f402a30800f6cf215
Maxim <goishikmg@tut.by>
2026-01-29 21:22:48 +0300

    Remove nullls

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -1,13 +1,13 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -21,22 +21,22 @@ require("lazy").setup({
         }
     },
     {
-		"nvim-neo-tree/neo-tree.nvim",
-		branch = "v3.x",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-			"MunifTanjim/nui.nvim",
-			-- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-		},
-	},
+        "nvim-neo-tree/neo-tree.nvim",
+        branch = "v3.x",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+            "MunifTanjim/nui.nvim",
+            -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+        },
+    },
     {
         'nvim-treesitter/nvim-treesitter',
         lazy = false,
         build = ':TSUpdate',
     },
-	{'neovim/nvim-lspconfig'},
- 	{'joshdick/onedark.vim'},
+    {'neovim/nvim-lspconfig'},
+    {'joshdick/onedark.vim'},
     {
         'rebelot/kanagawa.nvim',
         'folke/tokyonight.nvim',
@@ -44,7 +44,6 @@ require("lazy").setup({
     {'s1n7ax/nvim-window-picker'},
     {'hrsh7th/cmp-nvim-lsp'}, {'hrsh7th/cmp-buffer'}, {'hrsh7th/cmp-path'},
     {'hrsh7th/cmp-cmdline'}, {'hrsh7th/nvim-cmp'}, {'lewis6991/gitsigns.nvim'},
-    {'jose-elias-alvarez/null-ls.nvim'},
     {'windwp/nvim-autopairs'},
     {'windwp/nvim-ts-autotag'},
     {'phaazon/hop.nvim'},
