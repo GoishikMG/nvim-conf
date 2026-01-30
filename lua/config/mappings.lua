6df09889bd13fc3c1989e21c33c9a218de6a9521
Maxim <goishikmg@tut.by>
2026-01-30 11:49:46 +0300

    Refactoring

--- lua/config/mappings.lua
+++ lua/config/mappings.lua
@@ -3,10 +3,35 @@ vim.g.mapleader = " "
 vim.keymap.set('n', '<leader>e', ':Neotree float reveal<CR>')
 vim.keymap.set('n', '<leader>E', ':Neotree left reveal<CR>')
 vim.keymap.set('n', '<leader>o', ':Neotree float git_status<CR>')
-
 vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle)
-
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
+
+-- Plugins 
+
+local hop = require('hop')
+local directions = require('hop.hint').HintDirection
+
+vim.keymap.set('', 'f', function()
+    hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true })
+end, {remap=true})
+vim.keymap.set('', 'F', function()
+    hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true })
+end, {remap=true})
+
+local builtin = require('telescope.builtin')
+
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fw', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fg', builtin.git_files, {})
+vim.keymap.set('n', '<Tab>', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+vim.keymap.set('n', '<leader>gb', builtin.git_branches, {})
+vim.keymap.set('n', '<leader>gc', builtin.git_commits, {})
+vim.keymap.set('n', '<leader>gs', builtin.git_status, {})
+vim.keymap.set('n', '<leader>ls', builtin.lsp_document_symbols, {})
+vim.keymap.set('n', 'gr', builtin.lsp_references, {noremap = true, silent = true})
+vim.keymap.set('n', 'gd', builtin.lsp_definitions, {noremap = true, silent = true})
+
