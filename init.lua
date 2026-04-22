9dd26e3aaf9a4fce3a56d999682738d822615673
Maxim <goishikmg@tut.by>
2026-04-22 10:30:08 +0300

    Remove neotree

--- init.lua
+++ init.lua
@@ -4,7 +4,6 @@ require("config.mappings")
 require("config.config")
 require("config.colors")
 
-require("plugins.neotree")
 require("plugins.treesitter")
 require("plugins.cmp")
 require("plugins.autopairs")
--- lua/config/mappings.lua
+++ lua/config/mappings.lua
@@ -6,9 +6,7 @@ vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
 -- Neotree
-vim.keymap.set('n', '<leader>e', ':Neotree float reveal<CR>')
-vim.keymap.set('n', '<leader>E', ':Neotree left reveal<CR>')
-vim.keymap.set('n', '<leader>o', ':Neotree float git_status<CR>')
+vim.keymap.set('n', '<leader>e', ':Ex<CR>')
 
 -- Hop
 local hop = require('hop')
